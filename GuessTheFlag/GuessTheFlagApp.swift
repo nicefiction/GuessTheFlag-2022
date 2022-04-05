59eff46ce7a5f0105ecd5f4f68842ffac1842da8
Olivier Van hamme <nicefiction@gmail.com>
2022-04-05 15:22:58 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Olivier Van hamme on 05/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
