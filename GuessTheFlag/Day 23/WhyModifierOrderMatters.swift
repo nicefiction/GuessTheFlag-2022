8c486c4dc32df278f43d4cf0a21c8491188cc7ff
Olivier Van hamme <nicefiction@gmail.com>
2022-04-07 10:37:02 +0200

    Completes Day 23.4: Why modifier order matters.

---
+++ GuessTheFlag/Day 23/WhyModifierOrderMatters.swift
@@ -0,0 +1,53 @@
+/// Source:
+/// https://www.hackingwithswift.com/books/ios-swiftui/why-modifier-order-matters
+
+import SwiftUI
+
+
+
+struct WhyModifierOrderMatters: View {
+    
+    // MARK: - COMPUTED PROPERTIES
+    
+    var body: some View {
+        
+        VStack {
+            Button {
+                print(type(of: self.body))
+                /// Prints:
+                // Button<ModifiedContent<ModifiedContent<Text, _BackgroundStyleModifier<Color>>, _FrameLayout>>
+                /// Each modifier creates a new struct with that modifier applied,
+                /// rather than just setting a property on the view.
+                /// Every time we modify a view
+                /// SwiftUI applies that modifier by using generics:
+                /// `ModifiedContent<OurThing, OurModifier>`
+            } label: {
+                Text("Hello, World!")
+                    .background(.red)
+                    .frame(width: 200,
+                           height: 200)
+            }
+            
+            Text("Hello again!")
+                .padding()
+                .background(.red)
+                .padding()
+                .background(.blue)
+                .padding()
+                .background(.indigo)
+        }
+    }
+}
+
+
+
+
+// MARK: - PREVIEWS
+
+struct WhyModifierOrderMatters_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        
+        WhyModifierOrderMatters()
+    }
+}
