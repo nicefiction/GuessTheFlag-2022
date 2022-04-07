976c3a6b79fe9f56bf31a65cf284b392cf4d8fd4
Olivier Van hamme <nicefiction@gmail.com>
2022-04-07 15:20:35 +0200

    Completes Day 23.10: Custom modifiers.

---
+++ GuessTheFlag/Day 23/CustomModifiers.swift
@@ -0,0 +1,111 @@
+/// Source:
+/// https://www.hackingwithswift.com/books/ios-swiftui/custom-modifiers
+/// TIP:
+/// Often folks wonder when it’s better to add a custom view modifier
+/// versus just adding a new method to `View`,
+/// and really it comes down to one main reason:
+/// custom view modifiers can have their own stored properties,
+/// whereas extensions to `View` cannot.
+
+import SwiftUI
+
+struct Watermark: ViewModifier {
+    
+    // MARK: - PROPERTIES
+    var text: String
+    
+    
+    
+    // MARK: - METHODS
+    func body(content: Content)
+    -> some View {
+        
+        ZStack(alignment: .bottomTrailing) {
+            
+            content
+            
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.indigo)
+        }
+    }
+}
+
+
+
+
+struct HeaderStyle: ViewModifier {
+    
+    // MARK: - METHODS
+    func body(content: Content)
+    -> some View {
+        
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.yellow)
+    }
+}
+
+
+
+
+
+extension View {
+    
+    func createCustomHeaderStyle()
+    -> some View {
+        
+        return modifier(HeaderStyle())
+    }
+    
+    
+    func addWatermark(using text: String)
+    -> some View {
+        
+        return modifier(Watermark(text: text))
+    }
+}
+
+
+
+
+
+struct CustomModifiers: View {
+    
+    // MARK: - COMPUTED PROPERTIES
+    
+    var body: some View {
+        
+        VStack(spacing: 0) {
+            Text("Hello, World!")
+                .modifier(HeaderStyle())
+
+            Text("Hello Again")
+                .createCustomHeaderStyle()
+            /// `content`:
+            Color
+                .red
+                .frame(width: 250,
+                       height: 300)
+                .addWatermark(using: "Dorothy Gale")
+        }
+    }
+}
+
+
+
+
+
+// MARK: - PREVIEWS
+
+struct CustomModifiers_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        
+        CustomModifiers()
+    }
+}
